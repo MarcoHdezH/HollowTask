a7e4b5177b4e9bdec97bf8f9a6f89e6da787d4a0
Marco Hernandez <marco7245@hotmail.com>
2023-04-20 22:25:17 -0600

    Iniciando Proyecto

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { conectarDB } from './config/db.js';
+
+const app = express();
+
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,()=>{
+    console.log(`Servidor Corriendo en el Puerto ${PORT}`);
+})
