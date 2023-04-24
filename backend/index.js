861e9291b0c8718f5fb066285605c7679ea9f8bb
Marco Hernandez <marco7245@hotmail.com>
2023-04-24 11:27:07 -0600

    Iniciando Routing

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { conectarDB } from './config/db.js';
+import router from './routes/usuarioRoutes.js';
 
 const app = express();
 
@@ -8,8 +9,11 @@ dotenv.config();
 
 conectarDB();
 
+//Routing
+app.use("/api/Usuarios",router);
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Servidor Corriendo en el Puerto ${PORT}`);
 })
